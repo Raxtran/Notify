1af8711e8736702f04504c6e91f9d77a4101750c
alex <visualapd@hotmail.com>
2018-01-23 17:20:08 +0100

    lpm

---
+++ Notify/Models/Producto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Notify.Models
+{
+    public class Producto
+    {
+        [Key]
+        public int codigo;
+        public String nombre;
+        public int precio;
+
+    }
+}
---
+++ Notify/Models/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Notify.Models
+{
+    public class Usuario
+    {
+
+        public String idcorreo;
+        public String nombre;
+        public String contraseña; 
+
+    }
+}
