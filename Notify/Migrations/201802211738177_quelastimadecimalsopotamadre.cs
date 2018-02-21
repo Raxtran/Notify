70a98352d18f3d613971d1dfe2cd38ee20a4cc61
alex <visualapd@hotmail.com>
2018-02-21 18:40:45 +0100

    migraciones MUY necesarias

---
+++ Notify/Migrations/201802211738177_quelastimadecimalsopotamadre.cs
@@ -0,0 +1,16 @@
+namespace Notify.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class quelastimadecimalsopotamadre : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
